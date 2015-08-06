f4c47a5f2d48b0de8dcd7bfd31ebc60c9e9a6bb2
vravi17 <vishaakravi17@gmail.com>
2015-08-05 23:02:13 -0500

    First

---
+++ Philsosophy/Philsosophy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Philsosophy
+//
+//  Created by Rock Fort on 8/4/15.
+//  Copyright (c) 2015 Vishaak Ravi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
